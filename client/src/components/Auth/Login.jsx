61da78976544731bb44583002523b2a4727fba10
Nahuel61920 <estebancarrizo619@gmail.com>
2022-09-12 01:45:23 -0300

    Login/signIn

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from "react-router-dom";
 
 function Login() {
+    const [user, stateUser] = useState({
+        email: "",
+        password: ""
+    })
+
+    // extraer de user
+    const { email, password } = user;
+
+    function onChageLog(e) {
+        stateUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function onSubmitLog(e) {
+        e.preventDefault()
+    }
     return (
-        <h1>Login</h1>
+        <div className='form-usuario'>
+            <div className='contenedor-form sombra-dark'>
+                <h1>Login</h1>
+
+                <form onSubmit={onSubmitLog}>
+                    <div className='campo-form'>
+                        <label htmlFor='email'>Email</label>
+                        <input
+                            type="email"
+                            id='email'
+                            name='email'
+                            placeholder='Email'
+                            value={email}
+                            onChange={onChageLog}
+                        />
+                    </div>
+                    <div className='campo-form'>
+                        <label htmlFor='password'>Password</label>
+                        <input
+                            type="password"
+                            id='password'
+                            name='password'
+                            placeholder='Password'
+                            value={password}
+                            onChange={onChageLog}
+                        />
+                    </div>
+                    <div className='campo-form'>
+                        <input
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Login"
+                        />
+                    </div>
+                </form>
+                <Link to={"/sign-in"} className="enlace-cuenta">
+                    Sign In
+                </Link>
+            </div>
+        </div >
     )
 }
 
--- client/src/components/Auth/SignIn.jsx
+++ client/src/components/Auth/SignIn.jsx
@@ -1,8 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from "react-router-dom";
 
 function SignIn() {
+    const [user, stateUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirm: ""
+    })
+
+    // extraer de user
+    const { name, email, password, confirm } = user;
+
+    function onChageLog(e) {
+        stateUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function onSubmitLog(e) {
+        e.preventDefault()
+    }
     return (
-        <h1>Sign In</h1>
+        <div className='form-usuario'>
+            <div className='contenedor-form sombra-dark'>
+                <h1>SignIn</h1>
+
+                <form onSubmit={onSubmitLog}>
+                    <div className='campo-form'>
+                        <label htmlFor='name'>Name</label>
+                        <input
+                            type="text"
+                            id='name'
+                            name='name'
+                            placeholder='Name'
+                            value={name}
+                            onChange={onChageLog}
+                        />
+                    </div>
+                    <div className='campo-form'>
+                        <label htmlFor='email'>Email</label>
+                        <input
+                            type="email"
+                            id='email'
+                            name='email'
+                            placeholder='Email'
+                            value={email}
+                            onChange={onChageLog}
+                        />
+                    </div>
+                    <div className='campo-form'>
+                        <label htmlFor='password'>Password</label>
+                        <input
+                            type="password"
+                            id='password'
+                            name='password'
+                            placeholder='Password'
+                            value={password}
+                            onChange={onChageLog}
+                        />
+                    </div>
+                    <div className='campo-form'>
+                        <label htmlFor='confirm'>Confirm Password</label>
+                        <input
+                            type="password"
+                            id='confirm'
+                            name='confirm'
+                            placeholder='Confirm Password'
+                            value={confirm}
+                            onChange={onChageLog}
+                        />
+                    </div>
+                    <div className='campo-form'>
+                        <input
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="SignIn"
+                        />
+                    </div>
+                </form>
+                <Link to={"/"} className="enlace-cuenta">
+                    Login
+                </Link>
+            </div>
+        </div >
     )
 }
 
