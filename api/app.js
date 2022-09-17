35d4910624c15cde73660ceb3040c6928ba9f8bd
Nahuel61920 <estebancarrizo619@gmail.com>
2022-09-16 23:49:24 -0300

    schemaUser and create users

--- api/app.js
+++ api/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const conectarDB = require('./config/db');
 
 // Crando servidor
 const app = express();
 
+// Conectar a la base de datos
+conectarDB();
+
+// Habilitar express.json
+app.use(express.json({ extended: true }));
+
 const PORT = process.env.PORT || 3001;
+
+// importar rutas
+app.use('/users', require('./routes/users'));
 
 
 // arrancar servidor
---
+++ api/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UsersSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    register: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('User', UsersSchema);
